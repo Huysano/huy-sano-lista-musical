00d56a6ceeadb174c3b7da2ba3cb2e80b70c5695
Huysano <neotas1005@hotmail.com>
2026-08-31 08:02:00 -0500

    Actualizar caché de acceso a Drive

--- app/src/main/assets/service-worker.js
+++ app/src/main/assets/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "huy-sano-v13";
+const CACHE = "huy-sano-v14";
 const APP = ["./", "index.html", "manifest.webmanifest", "icon.svg", "google-config.js", "drive-sync.js", "privacy.html", "terms.html"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
